9f5adba536a71b62a08e285dd64db5422855bc67
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-07-09 22:53:27 +0300

    app added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from "react";
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Leftbar from "./components/Leftbar";
+import Home from "./pages/Home/Home";
+import Rightbar from "./components/Rightbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className="flex">
+          <div className="items-start    md:ml-20 min-w-[6rem] md:min-w-[6rem] xl:min-w-[18rem]">
+            <Leftbar />
+          </div>
+          <div className="flex flex-auto justify-between">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+            <Rightbar className="overflow-auto" />
+          </div>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
