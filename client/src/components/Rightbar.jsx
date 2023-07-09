0495a8e8b9a99c9fd342a42b8e7a9d840632bef5
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-07-09 22:54:55 +0300

    rightbar added

---
+++ client/src/components/Rightbar.jsx
@@ -0,0 +1,63 @@
+import { BsSearch } from "react-icons/bs";
+
+export default function Rightbar() {
+  return (
+    <div className="hidden lg:inline-block p-6 grow md:py-1  ">
+      <div className=" w-[22rem]  flex items-center bg-gray-100 rounded-full p-3 cursor-text focus:bg-white ">
+        <BsSearch className="h-5 w-5 cursor-default focus:bg-white" />
+        <input
+          type="text"
+          placeholder="Twitter'da Ara"
+          className="border-none bg-transparent ml-5 focus:bg-white focus:border-none "
+        />
+      </div>
+
+      <div className="mt-4 bg-gray-100 p-5  w-[22rem] rounded-xl">
+        <h3 className="text-xl text-black font-bold font-[inherit]">
+          İlgini çekebilecek gündemler
+        </h3>
+
+        <div className="mt-7">
+          <p className="text-xs text-gray-600">Türkiye tarihinde gündemde</p>
+          <h3 className="text-base text-black">Asgari</h3>
+          <p className="text-xs text-gray-600">27.3k Tweet</p>
+        </div>
+        <div className="mt-7">
+          <p className="text-xs text-gray-600">Türkiye tarihinde gündemde</p>
+          <h3 className="text-base text-black">Asgari</h3>
+          <p className="text-xs text-gray-600">27.3k Tweet</p>
+        </div>
+        <div className="mt-7">
+          <p className="text-xs text-gray-600">Türkiye tarihinde gündemde</p>
+          <h3 className="text-base text-black">Asgari</h3>
+          <p className="text-xs text-gray-600">27.3k Tweet</p>
+        </div>
+        <div className="mt-7">
+          <p className="text-xs text-gray-600">Türkiye tarihinde gündemde</p>
+          <h3 className="text-base text-black">Asgari</h3>
+          <p className="text-xs text-gray-600">27.3k Tweet</p>
+        </div>
+        <div className="mt-7">
+          <p className="text-xs text-gray-600">Türkiye tarihinde gündemde</p>
+          <h3 className="text-base text-black">Asgari</h3>
+          <p className="text-xs text-gray-600">27.3k Tweet</p>
+        </div>
+        <div className="mt-7">
+          <p className="text-xs text-gray-600">Türkiye tarihinde gündemde</p>
+          <h3 className="text-base text-black">Asgari</h3>
+          <p className="text-xs text-gray-600">27.3k Tweet</p>
+        </div>
+        <div className="mt-7">
+          <p className="text-xs text-gray-600">Türkiye tarihinde gündemde</p>
+          <h3 className="text-base text-black">Asgari</h3>
+          <p className="text-xs text-gray-600">27.3k Tweet</p>
+        </div>
+        <div className="mt-7">
+          <p className="text-xs text-gray-600">Türkiye tarihinde gündemde</p>
+          <h3 className="text-base text-black">Asgari</h3>
+          <p className="text-xs text-gray-600">27.3k Tweet</p>
+        </div>
+      </div>
+    </div>
+  );
+}
