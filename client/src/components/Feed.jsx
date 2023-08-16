19bfdc069d5efd4c2c41f85fba010bb3a4d978fe
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-08-16 18:45:33 +0300

    feed update

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -9,6 +9,34 @@ import { TbCalendarTime } from "react-icons/tb";
 import Post from "./Post";
 
 export default function Feed() {
+  const [tweetContent, setTweetContent] = useState("");
+
+  const handleTweet = async () => {
+    try {
+      const response = await axios.post("localhost:9000/api/tweet/", {
+        tweetContent,
+      });
+      // Handle successful tweet submission (e.g., show a success message)
+      console.log("Tweet posted:", response.data);
+      // Clear the tweet content after posting
+      setTweetContent(response.data);
+    } catch (error) {
+      // Handle error (e.g., display an error message)
+      console.error("Error posting tweet:", error);
+    }
+  };
+
+  const getTweet = async () => {
+    axios.get("localhost:9000/api/tweet/").then((response) => {
+      console.log(response);
+    });
+  };
+  useEffect(() => {
+    axios.get("http://localhost:9000/api/tweet/").then((response) => {
+      console.log(response);
+    });
+  }, []);
+
   return (
     <form className="mt-4 mx-8 ">
       <div className="flex focus-within:border-b-[1px]">
@@ -20,7 +48,8 @@ export default function Feed() {
           />
         </div>
         <input
-          // value={tweet.text}
+          value={tweetContent}
+          onChange={(e) => setTweetContent(e.target.value)}
           placeholder="Neler oluyor?"
           type="text"
           className="mx-3 text-xl pb-10 pr-52 outline-none text-left"
@@ -51,6 +80,8 @@ export default function Feed() {
           <button
             // onClick={sendTweet}
             className="bg-blue-300 text-white py-2 px-5 rounded-full font-bold"
+            onClick={handleTweet}
+            disabled={!tweetContent.trim()} // Disable button when tweetContent is empty or whitespace only
           >
             Tweetle
           </button>
@@ -63,6 +94,7 @@ export default function Feed() {
           </div>
         );
       })} */}
+      <Post />
     </form>
   );
 }
