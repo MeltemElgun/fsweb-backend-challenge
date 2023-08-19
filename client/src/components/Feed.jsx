02e7afe754af2b627a343f9a954831612ed149d6
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-08-20 00:06:52 +0300

    feed- tweet  localTokenCheck , tweet handle , get tweet, tweet map

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -9,92 +9,136 @@ import { TbCalendarTime } from "react-icons/tb";
 import Post from "./Post";
 
 export default function Feed() {
-  const [tweetContent, setTweetContent] = useState("");
-
+  const [tweetContent, setTweetContent] = useState({ content: "", image: "" });
+  const [tweets, setTweets] = useState([]);
+  const [localToken, setLocalToken] = useState(
+    JSON.parse(localStorage.getItem("user"))
+  );
+  JSON.parse(localStorage.getItem("user"));
+  console.log();
+  const localTokenCheck = async () => {
+    await axios
+      .get("http://localhost:9000/api/auth", {
+        headers: {
+          Authorization: `${localToken?.token}`,
+        },
+      })
+      .then((res) => {
+        res.data && setLocalToken(JSON.parse(localStorage.getItem("user")));
+        // console.log(JSON.stringify(res));
+      });
+  };
+  useEffect(() => {
+    localToken && localTokenCheck();
+    getTweet();
+  }, []);
   const handleTweet = async () => {
     try {
-      const response = await axios.post("localhost:9000/api/tweet/", {
-        tweetContent,
-      });
-      // Handle successful tweet submission (e.g., show a success message)
-      console.log("Tweet posted:", response.data);
-      // Clear the tweet content after posting
-      setTweetContent(response.data);
+      await axios
+        .post(
+          "http://localhost:9000/api/tweet/",
+          {
+            content: tweetContent.content,
+            image: tweetContent.image,
+            userId: 2,
+          },
+          {
+            headers: {
+              Authorization: `${localToken?.token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => {
+          setTweetContent(response.data);
+
+          console.log(JSON.stringify(response.data));
+        });
     } catch (error) {
       // Handle error (e.g., display an error message)
-      console.error("Error posting tweet:", error);
+      console.log(error);
+    }
+  };
+  const getTweet = async () => {
+    try {
+      await axios
+        .get("http://localhost:9000/api/tweet/", {
+          headers: {
+            Authorization: `${localToken?.token}`,
+            "Content-Type": "application/json",
+          },
+        })
+        .then((response) => {
+          setTweets(response.data);
+          console.log(JSON.stringify(response.data));
+        });
+    } catch (error) {
+      // Handle error (e.g., display an error message)
+      console.log(error);
     }
   };
 
-  const getTweet = async () => {
-    axios.get("localhost:9000/api/tweet/").then((response) => {
-      console.log(response);
-    });
-  };
-  useEffect(() => {
-    axios.get("http://localhost:9000/api/tweet/").then((response) => {
-      console.log(response);
-    });
-  }, []);
-
   return (
-    <form className="mt-4 mx-8 ">
-      <div className="flex focus-within:border-b-[1px]">
-        <div>
-          <img
-            className="inline-block h-10 w-10 rounded-full"
-            src="../ben.jpg"
-            alt=""
+    <>
+      <form className="mt-4 mx-8 ">
+        <div className="flex focus-within:border-b-[1px]">
+          <div>
+            <img
+              className="inline-block h-10 w-10 rounded-full"
+              src="../ben.jpg"
+              alt=""
+            />
+          </div>
+          <input
+            value={tweetContent.content}
+            onChange={(e) =>
+              setTweetContent({ ...tweetContent, content: e.target.value })
+            }
+            placeholder="Neler oluyor?"
+            type="text"
+            className="mx-3 text-xl pb-10 pr-52 outline-none text-left"
           />
+          {tweetContent.image && <img src={tweetContent.image} alt="" />}
         </div>
-        <input
-          value={tweetContent}
-          onChange={(e) => setTweetContent(e.target.value)}
-          placeholder="Neler oluyor?"
-          type="text"
-          className="mx-3 text-xl pb-10 pr-52 outline-none text-left"
-        />
-      </div>
-      <div className="flex items-center justify-between  mt-2 pl-20 pr-4 pb-4">
-        <ul className="flex space-x-5">
-          <li>
-            <BsImage className="text-[#1d9bf0] w-4 h-4" />
-          </li>
-          <li>
-            <AiOutlineFileGif className="text-[#1d9bf0] w-5 h-5" />
-          </li>
-          <li className="hidden md:block">
-            <FaPoll className=" text-[#1d9bf0] w-4 h-4" />
-          </li>
-          <li>
-            <BsEmojiSmile className="text-[#1d9bf0] w-4 h-4" />
-          </li>
-          <li className="hidden md:block">
-            <TbCalendarTime className="text-[#1d9bf0] w-5 h-5" />
-          </li>
-          <li>
-            <IoLocationSharp className="text-[#1d9bf0] w-5 h-5" />
-          </li>
-        </ul>
-        <div className="">
+        <div className="flex items-center justify-between  mt-2 pl-20 pr-4 pb-4">
+          <ul className="flex space-x-5">
+            <li>
+              <BsImage className="text-[#1d9bf0] w-4 h-4" />
+            </li>
+            <li>
+              <AiOutlineFileGif className="text-[#1d9bf0] w-5 h-5" />
+            </li>
+            <li className="hidden md:block">
+              <FaPoll className=" text-[#1d9bf0] w-4 h-4" />
+            </li>
+            <li>
+              <BsEmojiSmile className="text-[#1d9bf0] w-4 h-4" />
+            </li>
+            <li className="hidden md:block">
+              <TbCalendarTime className="text-[#1d9bf0] w-5 h-5" />
+            </li>
+            <li>
+              <IoLocationSharp className="text-[#1d9bf0] w-5 h-5" />
+            </li>
+          </ul>
+
           <button
-            // onClick={sendTweet}
-            className="bg-blue-300 text-white py-2 px-5 rounded-full font-bold"
             onClick={handleTweet}
-            disabled={!tweetContent.trim()} // Disable button when tweetContent is empty or whitespace only
+            type="submit"
+            className="bg-blue-400 text-white py-2 px-5 rounded-full font-bold"
+            // disabled={!tweetContent.trim()} // Disable button when tweetContent is empty or whitespace only
           >
-            Tweetle
+            Gönder
           </button>
-        </div>
-      </div>
-      {/* {tweets.map((tweet) => {
+        </div>{" "}
+      </form>
+      {tweets.map((tweet) => {
         return (
           <div key={tweet.id}>
             <Post props={tweet} />
           </div>
         );
-      })} */}
-      <Post />
-    </form>
+      })}
+    </>
   );
 }
