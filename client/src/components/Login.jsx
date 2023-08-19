d9bbe85a75570adf85193c2c23b1652f38821af8
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-08-20 00:08:03 +0300

    login handle loginIn post  , className update

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,38 +1,42 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  let navigate = useNavigate();
+  const [userData, setUserData] = useState({ username: "", password: "" });
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm({
-    mode: "onChange",
-    resolver: yupResolver(validationSchema),
-  });
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
 
-  const onSubmit = async (data) => {
+  const API_URL = "http://localhost:9000/api/auth/login";
+
+  const handleLogIn = async (e) => {
+    e.preventDefault();
     try {
       await axios
-        .post("http://localhost:9000/api/auth/login", {
-          username: data.username,
-          password: data.password,
-        })
-        .then((res) => localStorage.setItem("user", JSON.stringify(res.data)));
-      navigate("/dashboard");
+        .post(
+          API_URL,
+          {
+            username: userData.username,
+            passhash: userData.password,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => {
+          localStorage.setItem("user", JSON.stringify(response.data)),
+            console.log(JSON.stringify(response.data));
+        });
+      navigate("/");
       closeModal();
-      toast.success("Giriş yapıldı");
     } catch (error) {
-      console.error(error);
-      toast.error("Giriş yapılamadı");
-      // Hata işleme
+      console.log(error);
     }
   };
-
   const openModal = () => {
     setShowModal(true);
   };
@@ -40,58 +44,47 @@ export default function Login() {
   const closeModal = () => {
     setShowModal(false);
   };
-  const handleLogout = () => {
-    localStorage.removeItem("user"); // Kullanıcıyı localStorage'dan kaldır
-    setIsLoggedIn(false); // isLoggedIn öğesini false olarak ayarlayın
-    navigate("/"); // çıkış yapınca ana sayfaya yönlendir
-    toast.info("Çıkış yapıldı");
-  };
-
-  const [localToken, setLocalToken] = useState(
-    JSON.parse(localStorage.getItem("user"))
-  );
-  const localTokenCheck = async () => {
-    await axios
-      .get("localhost:9000/api/auth/register", {
-        headers: {
-          Authorization: `${localToken?.token}`,
-        },
-      })
-      .then((res) => {
-        res.data && setLocalToken(JSON.parse(localStorage.getItem("user")));
-        setIsLoggedIn(true);
-      });
-  };
-
-  useEffect(() => {
-    localToken && localTokenCheck();
-  }, []);
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <img src="/icons/twitter.png" alt="" />
+    <form
+      onSubmit={handleLogIn}
+      className="min-w-[20rem] max-h-48 sm:min-w-[35rem]  lg:min-w-[38rem]  mt-3 ml-10 flex flex-col border-2  rounded-l p-20  px-4 pt-5 pb-4 sm:p-6 sm:pb-9  items-center justify-center min-h-[28rem] bg-gray-100    rounded-md shadow-md w-full max-w-sm"
+    >
+      {/* <img src="/icons/twitter.png" alt="" /> */}
 
-      <h1>Log In</h1>
+      <h1 className=" w-full mb-2 px-3 py-2">Tweter'a giriş yap</h1>
 
       <input
         type="text"
-        placeholder="username"
-        {...register("username", { required: true })}
+        placeholder="Kullanıcı adı"
+        value={userData.username}
+        onChange={(e) => setUserData({ ...userData, username: e.target.value })}
+        className="w-full mb-2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
       />
-      {errors.username && <p>This field is required</p>}
 
       <input
         type="password"
-        placeholder="password"
-        {...register("password", { required: true })}
+        placeholder="Şifre"
+        value={userData.password}
+        onChange={(e) => setUserData({ ...userData, password: e.target.value })}
+        className="w-full mb-2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
       />
-      {errors.password && <p>This field is required</p>}
 
-      <button type="submit">Log In</button>
+      <button
+        className="w-full bg-blue-500 text-white rounded-full py-2 px-4 text-lg font-semibold focus:outline-none hover:bg-blue-600 transition duration-300 ease-in-out"
+        type="submit"
+        disabled={!userData.username || !userData.password}
+      >
+        Giriş yap
+      </button>
 
-      <div className={styles.router}>
-        Don't have an account?{" "}
-        <span onClick={() => navigate("/signup")}>Sign up</span>
+      <div className="mt-4 text-sm text-gray-600">
+        Henüz bir hesabınız yokmu?{" "}
+        <span
+          class="text-blue-500 cursor-pointer"
+          onClick={() => navigate("/signup")}
+        >
+          Sign up
+        </span>
       </div>
     </form>
   );
