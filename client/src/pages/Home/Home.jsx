8acf8ded1466db5247b1b11096d95167c57da6f0
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-08-20 00:10:17 +0300

    clasName update

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { useEffect } from "react";
 import Feed from "../../components/Feed";
-
+import { useNavigate } from "react-router-dom";
+import Login from "../../components/Login";
 const Home = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const navigate = useNavigate();
+  // useEffect(() => {
+  //   if (!user) {
+  //     return navigate("/login");
+  //   } else {
+  //     return navigate("/");
+  //   }
+  // }, [user, navigate]);
+
   return (
     <>
-      <div>
-        <div className="flex flex-col border-x-[1px]">
-          <div className="min-w-[39rem] sm:min-w-[37rem] lg:min-w-[36rem] xl:w-[38rem]">
-            <div className="mt-3 ml-1 flex flex-col border-b-[1px]">
+      <div className="border-r-[1px] ">
+        <div className="flex flex-col border-b-[1px] ">
+          <div className="min-w-[39rem] sm:min-w-[37rem] lg:min-w-[36rem] xl:w-[38rem] ">
+            <div className="mt-3 ml-1 flex flex-col ">
               <p className="font-bold text-gray-800 text-xl ml-4">Anasayfa</p>
               <div className="flex mt-5">
                 <a
