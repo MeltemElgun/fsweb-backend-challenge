0166994dae0973a0c8306f0ccd321286ddec53b0
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-09-03 21:10:30 +0300

    feed settweetcontent update

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -7,15 +7,20 @@ import { IoLocationSharp } from "react-icons/io5";
 import { FaPoll } from "react-icons/fa";
 import { TbCalendarTime } from "react-icons/tb";
 import Post from "./Post";
-
+import { useNavigate } from "react-router-dom";
 export default function Feed() {
-  const [tweetContent, setTweetContent] = useState({ content: "", image: "" });
+  const [tweetContent, setTweetContent] = useState({
+    content: "",
+    image: "",
+  });
   const [tweets, setTweets] = useState([]);
+
   const [localToken, setLocalToken] = useState(
     JSON.parse(localStorage.getItem("user"))
   );
+
   JSON.parse(localStorage.getItem("user"));
-  console.log();
+  const navigate = useNavigate();
   const localTokenCheck = async () => {
     await axios
       .get("https://twitter-backend-ac6l.onrender.com/api/auth", {
@@ -25,9 +30,10 @@ export default function Feed() {
       })
       .then((res) => {
         res.data && setLocalToken(JSON.parse(localStorage.getItem("user")));
-        // console.log(JSON.stringify(res));
+        console.log(JSON.stringify(res.data));
       });
   };
+
   useEffect(() => {
     localToken && localTokenCheck();
     getTweet();
@@ -40,7 +46,7 @@ export default function Feed() {
           {
             content: tweetContent.content,
             image: tweetContent.image,
-            userId: 2,
+            userId: 1,
           },
           {
             headers: {
@@ -50,7 +56,7 @@ export default function Feed() {
           }
         )
         .then((response) => {
-          setTweetContent(response.data);
+          setTweetContent([...tweets, response.data]);
 
           console.log(JSON.stringify(response.data));
         });
@@ -59,6 +65,7 @@ export default function Feed() {
       console.log(error);
     }
   };
+
   // http://localhost:9000
   const getTweet = async () => {
     try {
@@ -81,62 +88,68 @@ export default function Feed() {
 
   return (
     <>
-      <form className="mt-4 mx-8 ">
-        <div className="flex focus-within:border-b-[1px]">
-          <div>
-            <img
-              className="inline-block h-10 w-10 rounded-full"
-              src="../ben.jpg"
-              alt=""
-            />
-          </div>
-          <input
-            value={tweetContent.content}
-            onChange={(e) =>
-              setTweetContent({ ...tweetContent, content: e.target.value })
-            }
-            placeholder="Neler oluyor?"
-            type="text"
-            className="mx-3 text-xl pb-10 pr-52 outline-none text-left"
-          />
-          {tweetContent.image && <img src={tweetContent.image} alt="" />}
-        </div>
-        <div className="flex items-center justify-between  mt-2 pl-20 pr-4 pb-4">
-          <ul className="flex space-x-5">
-            <li>
-              <BsImage className="text-[#1d9bf0] w-4 h-4" />
-            </li>
-            <li>
-              <AiOutlineFileGif className="text-[#1d9bf0] w-5 h-5" />
-            </li>
-            <li className="hidden md:block">
-              <FaPoll className=" text-[#1d9bf0] w-4 h-4" />
-            </li>
-            <li>
-              <BsEmojiSmile className="text-[#1d9bf0] w-4 h-4" />
-            </li>
-            <li className="hidden md:block">
-              <TbCalendarTime className="text-[#1d9bf0] w-5 h-5" />
-            </li>
-            <li>
-              <IoLocationSharp className="text-[#1d9bf0] w-5 h-5" />
-            </li>
-          </ul>
+      {localToken ? (
+        <>
+          <form className="mt-4 mx-8 ">
+            <div className="flex focus-within:border-b-[1px]">
+              <div>
+                <img
+                  className="inline-block h-10 w-10 rounded-full"
+                  src="../ben.jpg"
+                  alt=""
+                />
+              </div>
+              <input
+                value={tweetContent.content}
+                onChange={(e) =>
+                  setTweetContent({ ...tweetContent, content: e.target.value })
+                }
+                placeholder="Neler oluyor?"
+                type="text"
+                className="mx-3 text-xl pb-10 pr-52 outline-none text-left"
+              />
+              {tweetContent.image && <img src={tweetContent.image} alt="" />}
+            </div>
+            <div className="flex items-center justify-between  mt-2 pl-20 pr-4 pb-4">
+              <ul className="flex space-x-5">
+                <li>
+                  <BsImage className="text-[#1d9bf0] w-4 h-4" />
+                </li>
+                <li>
+                  <AiOutlineFileGif className="text-[#1d9bf0] w-5 h-5" />
+                </li>
+                <li className="hidden md:block">
+                  <FaPoll className=" text-[#1d9bf0] w-4 h-4" />
+                </li>
+                <li>
+                  <BsEmojiSmile className="text-[#1d9bf0] w-4 h-4" />
+                </li>
+                <li className="hidden md:block">
+                  <TbCalendarTime className="text-[#1d9bf0] w-5 h-5" />
+                </li>
+                <li>
+                  <IoLocationSharp className="text-[#1d9bf0] w-5 h-5" />
+                </li>
+              </ul>
 
-          <button
-            onClick={handleTweet}
-            type="submit"
-            className="bg-blue-400 text-white py-2 px-5 rounded-full font-bold"
-            // disabled={!tweetContent.trim()} // Disable button when tweetContent is empty or whitespace only
-          >
-            Gönder
-          </button>
-        </div>{" "}
-      </form>
+              <button
+                onClick={handleTweet}
+                type="submit"
+                className="bg-blue-400 text-white py-2 px-5 rounded-full font-bold"
+                // disabled={!tweetContent.trim()} // Disable button when tweetContent is empty or whitespace only
+              >
+                Gönder
+              </button>
+            </div>{" "}
+          </form>
+        </>
+      ) : (
+        <div>{navigate("/")};</div>
+      )}{" "}
       {tweets.map((tweet) => {
         return (
           <div key={tweet.id}>
-            <Post props={tweet} />
+            <Post props={tweet} localToken={localToken} />
           </div>
         );
       })}
