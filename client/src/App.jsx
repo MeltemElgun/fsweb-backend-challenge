095b2b2ef95db0783ef576cec640ad8128e7a015
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2024-04-05 15:53:19 +0300

    update app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,25 +2,28 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Leftbar from "./components/Leftbar";
-import Home from "./pages/Home/Home";
+import Home from "./pages/Home";
 import Rightbar from "./components/Rightbar";
 import Login from "./components/Login";
-
+import Profile from "./pages/Profile";
+import PProfile from "./pages/PProfile";
 function App() {
   return (
     <>
       <BrowserRouter>
         <div className="flex">
-          <div className="items-start    md:ml-20 min-w-[6rem] md:min-w-[6rem] xl:min-w-[18rem]">
+          <div className="items-start border-r-[1px]  md:ml-[1rem] lg:ml-[3rem]  min-w-[6rem] md:min-w-[6rem] xl:min-w-[19rem]">
             <Leftbar />
           </div>
-          <div className="flex flex-auto justify-between border-r-[1px] ">
+          <div className="flex  flex-col justify-between border-r-[1px]  ">
             <Routes>
               <Route path="/login" element={<Login />} />
               <Route path="/" element={<Home />} />
-            </Routes>
-            <Rightbar className="overflow-auto" />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/pprofile" element={<PProfile />} />
+            </Routes>{" "}
           </div>
+          <Rightbar className="overflow-auto" />
         </div>
       </BrowserRouter>
     </>
